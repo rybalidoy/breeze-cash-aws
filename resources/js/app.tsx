36f563bb0193a2090316a9ddfa074d694596aae0
rybalidoy <rybalidoy@gmail.com>
2024-09-14 13:57:57 +0800

    setup react + typescript + tailwind

---
+++ resources/js/app.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="p-5 font-semibold">Ryan</h1>
+    </>
+  );
+}
+
+export default App;
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/{any?}', function () {
+  return view('app');
+});
