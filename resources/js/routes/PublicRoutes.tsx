7e7edd4623f4733543c6dc3b5aa5f5cd1cd7ec3a
rybalidoy <rybalidoy@gmail.com>
2024-09-22 03:08:35 +0800

    setup template for routes

---
+++ resources/js/routes/PublicRoutes.tsx
@@ -0,0 +1,8 @@
+
+
+export const PublicRoutes = Object.freeze([
+  { path: '/login', component: null },
+  { path: '/register', component: null },
+  { path: '/forgot-password', component: null },
+  { path: '/reset-password', component: null },
+]);
